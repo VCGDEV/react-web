1ef27ccc14d50488b6c75d02c84744410495e81c
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-04-29 15:59:31 -0500

    added data table component

---
+++ src/layout/table/container/table-container.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class TableContainer extends React.Component {
+  render() {
+    return "<h1>A table container</h1>";
+  }
+}
+
+export default TableContainer;
--- src/pages/customers/customer-list.jsx
+++ src/pages/customers/customer-list.jsx
@@ -1,10 +1,64 @@
 import React from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 class CustomerList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [
+        {
+          headerName: "Make",
+          field: "make",
+          sortable: true,
+          filter: true
+        },
+        {
+          headerName: "Model",
+          field: "model",
+          sortable: true,
+          filter: true
+        },
+        {
+          headerName: "Price",
+          field: "price",
+          sortable: true,
+          filter: true
+        }
+      ],
+      rowData: [
+        {
+          make: "Toyota",
+          model: "Celica",
+          price: 35000
+        },
+        {
+          make: "Ford",
+          model: "Mondeo",
+          price: 32000
+        },
+        {
+          make: "Porsche",
+          model: "Boxter",
+          price: 72000
+        }
+      ]
+    };
+  }
   render() {
     return (
-      <div>
-        <p>Customers</p>
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: "100%",
+          width: "100%"
+        }}
+      >
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}
+        />
       </div>
     );
   }
